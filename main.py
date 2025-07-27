69f5e9dba90bee46593af28a745d8b3aae691c45
vba1946 <vba1946@gmail.com>
2025-07-27 23:10:21 +0300

    add сycle сode

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-        pass
+        screen.fill(color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                    target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        # Перемещены screen.blit и pygame.display.update() внутрь цикла
+        screen.blit(target_img, (target_x, target_y))
+        pygame.display.update()
 
 pygame.quit()
+
